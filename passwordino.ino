6711a17d8f3c89c43679442435e96274777f5b4d
Federico Terzi <freddy6896@gmail.com>
2018-01-25 10:24:45 +0100

    Added buttons

--- passwordino.ino
+++ passwordino.ino
@@ -3,11 +3,29 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Bounce2.h>
 
+// Software version
 #define PASSWORDINO_VERSION "0.1"
 
+// Buttons
+int btnPins[] = {4,5,6,7,8,9};
+int btnValues[] = {1,2,3,4,5,6};
+#define NUM_BUTTONS 6
+Bounce dbtn[NUM_BUTTONS];
+
+// Display
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
+
+// Typing mode ( which method to use to inject keys )
+#define WIN_MODE 1  // More precise, but only works on windows.
+					// Inject keys by typing ALT + NUMPAD NUMBER
+					// to directly specify the ascii character.
+#define GEN_MODE 2   // Type letters using the standard USA keyboard
+					// layout. The problem is that if you use a non USA
+					// keyboard, all the symbols may appear incorrectly.
+int typeMode = GEN_MODE;
 
 //The setup function is called once at startup of the sketch
 void setup() {
@@ -28,11 +46,62 @@ void setup() {
 	display.println("*********************");
 	display.display();
 	delay(1);
+
+	// Set up the buttons
+	for (int i = 0; i< NUM_BUTTONS; i++) {
+		pinMode(btnPins[i], INPUT_PULLUP);
+
+		// Set up the debauncer
+		dbtn[i].attach(btnPins[i]);
+		dbtn[i].interval(5);
+	}
 }
 
 // The loop function is called in an endless loop
 void loop() {
-//Add your repeated code here
+	// Update the debauncer
+	for (int i = 0; i< NUM_BUTTONS; i++) {
+		dbtn[i].update();
+	}
+
+	// Check if a button was pressed
+	for (int i = 0; i< NUM_BUTTONS; i++) {
+		if (dbtn[i].fell()) {
+			handleBtnPress(btnValues[i]);
+		}
+	}
+
+	renderDisplay();
+}
+
+void handleBtnPress(int value) {
+	Serial.println(value);
+	if (value==4) {
+		typeMode = WIN_MODE;
+	}
+}
+
+void renderDisplay() {
+	display.clearDisplay();
+	display.setTextSize(1);
+	display.setTextColor(WHITE);
+	display.setCursor(0, 0);
+
+	// Print the topbar
+	display.print("MODE: ");
+	switch(typeMode) {
+		case WIN_MODE:
+			display.print("WIN");
+			break;
+		case GEN_MODE:
+			display.print("GEN");
+			break;
+	}
+	display.println();
+	display.drawLine(0, 8, display.width(), 8, WHITE);
+
+
+	display.display();
 }
 
 
